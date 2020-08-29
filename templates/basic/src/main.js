87e1eb69160d306565e2ae4c9bcc1be68edf68f8
Wachirawut Thamviset <twachikku@gmail.com>
2020-08-29 08:26:27 +0700

    Add files via upload

--- templates/basic/src/main.js
+++ templates/basic/src/main.js
@@ -5,16 +5,20 @@ import GameScene from "./scenes/game.js";
 var config = {
     type: Phaser.AUTO,
     width: 16*60,
-    height: 9*60,
+    height: 8*60,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 800 },
+            gravity: { y: 1000 },
             debug: true
         }
     },
     scene: [MenuScene, GameScene],
-    parent: "gamecanvas"
+    parent: "gamecanvas",
+    scale: {
+        mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    }
 };
 
 const game = new Phaser.Game(config);
--- templates/basic/src/scenes/game.js
+++ templates/basic/src/scenes/game.js
@@ -13,7 +13,10 @@ export default class GameScene extends Phaser.Scene{
     {
         const width = this.scale.width;
         const height = this.scale.height;
-         this.add.text(width/2,200,"(scenes/game.js) กดปุ่ม ESC เพื่อกลับ",{fontSize: 16,fontFamily: 'Bai Jamjuree'})
+
+        this.add.text(width/2,10,"BASIC GAME",{fontSize: 32,fontFamily: 'Monoton',color:'#FF0'}).setOrigin(0.5,0)
+
+        this.add.text(width/2,200,"(scenes/game.js) กดปุ่ม ESC เพื่อกลับ",{fontSize: 16,fontFamily: 'Bai Jamjuree'})
                 .setOrigin(0.5,0)
         this.input.keyboard.on('keydown-ESC',function(){
             this.scene.start("menu");  
